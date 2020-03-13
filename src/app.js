f2c91706c5939ec32f615c56358cd0b16d2b41a4
Abdur Rehman Khalid <abdur.r.khalid@gmail.com>
2020-03-13 23:06:48 +0500

    Made Some Changes to The Config Files.

--- src/app.js
+++ src/app.js
@@ -3,5 +3,4 @@ import React from 'react';
 import './styles/style.scss';
 import 'normalize.css/normalize.css';
 import GameInfoApp from './components/GameInfoApp';
-
 ReactDOM.render(<GameInfoApp />, document.getElementById("app"));
--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,34 @@
 const path = require('path');
 
-console.log(__dirname);
-module.exports = {
-     entry: './src/app.js',
-     output: {
-        path: __dirname,
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [{
-            loader: 'babel-loader',
-            test: /\.js$/,
-            exclude: /node_modules/
-        },
-        {
-            test: /\.s?css$/,
-            use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader'
-            ]
-        }]
-    },
-    devtool: 'cheap-module-eval-source-map',
-    devServer: {
-        contentBase: __dirname,
-        historyApiFallback: true
-    }
-}
+module.exports = (env) => {
+    
+    const isProdcution = env === 'production';
+
+    return{
+        entry: './src/app.js',
+        output: {
+           path: __dirname,
+           filename: 'bundle.js'
+       },
+       module: {
+           rules: [{
+               loader: 'babel-loader',
+               test: /\.js$/,
+               exclude: /node_modules/
+           },
+           {
+               test: /\.s?css$/,
+               use: [
+                   'style-loader',
+                   'css-loader',
+                   'sass-loader'
+               ]
+           }]
+       },
+       devtool: isProdcution ? 'source-map' : 'cheap-module-eval-source-map',
+       devServer: {
+           contentBase: __dirname,
+           historyApiFallback: true
+       }
+   }
+};
